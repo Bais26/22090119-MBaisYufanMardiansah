7d28af093f1a1cc5262a575547741594cfc5fa4b
Bais26 <baisyufan2004@gmail.com>
2023-10-10 12:29:15 +0700

    first commit

---
+++ src/pkg22090119/mbaisyufanmardiansah/MBaisYufanMardiansah.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkg22090119.mbaisyufanmardiansah;
+
+/**
+ *
+ * @author bass
+ */
+public class MBaisYufanMardiansah {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello Word");
+    }
+    
+}
